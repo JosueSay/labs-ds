a4f84841b8d3896dbb4a3bc082f452b73c1f103b
JosueSay <106031855+JosueSay@users.noreply.github.com>
2025-07-28 17:36:01 -0600

    feat: inicio lab3

---
+++ lab3/flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A basic multiplatform flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      python = pkgs.python312.withPackages (p: let
+        pmdarima = p.pmdarima.overrideAttrs (old: {
+          doCheck = false;
+          doInstallCheck = false;
+        });
+      in [
+        # python = pkgs.python3.withPackages (p: [
+        p.pandas
+        p.jupyterlab
+        p.numpy
+        p.matplotlib
+        p.statsmodels
+        p.datetime
+        p.openpyxl
+        p.seaborn
+        p.scipy
+        p.prophet
+        p.sklearn-compat
+        p.keras
+        p.tabulate
+        pmdarima
+      ]);
+    in {
+      default = pkgs.mkShell {
+        packages = [python pkgs.black];
+      };
+    });
+  };
+}
