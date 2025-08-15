ae6d8a455d6aeb8af77a775f498f720355788cc7
ElrohirGT <elrohirgt@gmail.com>
2025-08-14 22:56:24 -0600

    feat: Added nix env

---
+++ lab5/flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "A basic multiplatform flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      python = pkgs.python312.withPackages (p: let
+        pmdarima = p.pmdarima.overrideAttrs (old: {
+          doCheck = false;
+          doInstallCheck = false;
+        });
+        openeo = import ./openeo.nix {
+          pkgs = pkgs;
+          pyPkgs = p;
+        };
+      in [
+        openeo
+        p.pandas
+        p.jupyterlab
+        p.jupyterlab-execute-time
+        p.jupyterlab-widgets
+        p.jupyterlab-lsp
+        p.numpy
+        p.matplotlib
+        p.statsmodels
+        p.datetime
+        p.openpyxl
+        p.seaborn
+        p.scipy
+        p.prophet
+        p.sklearn-compat
+        p.keras
+        p.tabulate
+        p.geopandas
+        pmdarima
+      ]);
+    in {
+      default = pkgs.mkShell {
+        packages = [python pkgs.black];
+      };
+    });
+  };
+}
